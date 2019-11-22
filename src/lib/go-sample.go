a51c628610d884f260db2b42f6a3b64932f8f351
hanhan1978 <ryo.tomidokoro@gmail.com>
2019-11-22 13:24:53 +0900

    gofmt

--- src/lib/go-sample.go
+++ src/lib/go-sample.go
@@ -15,27 +15,27 @@ go build -buildmode=c-shared -o goarg.so goarg.go
 
 //export argInt
 func argInt(i int64) int64 {
-    return i
+	return i
 }
 
 //export argDouble
-func argDouble(d float64) float64{
-    return d
+func argDouble(d float64) float64 {
+	return d
 }
 
 //export argString
-func argString(s *C.char) *C.char{
-    return C.CString(C.GoString(s))
+func argString(s *C.char) *C.char {
+	return C.CString(C.GoString(s))
 }
 
 //export argStruct
-func argStruct(c *C.struct_animal) *C.struct_animal{
-    return c
+func argStruct(c *C.struct_animal) *C.struct_animal {
+	return c
 }
 
 //export argIntArray
 func argIntArray(s []int) unsafe.Pointer {
-    return unsafe.Pointer(&s[0])
+	return unsafe.Pointer(&s[0])
 }
 
-func main(){}
+func main() {}
